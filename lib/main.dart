0fcade80ed201b3bc853ce704bae6f3b03f4f7e7
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-09-11 22:35:07 +0300

    Make main function asynchronous to initialize services before running the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:MatchIn/app.dart';
+import 'package:MatchIn/core/services/services_locator.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupServiceLocator();
+
   runApp(const MatchIn());
 }
