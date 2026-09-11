7effa7c38662bac46f46c469262023e37d405178
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-09-11 22:01:04 +0300

    Refactor main application structure by replacing MyApp with MatchIn and moving app logic to a new app.dart file

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MatchIn extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
