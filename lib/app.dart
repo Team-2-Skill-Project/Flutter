3f4622ffc322cbfb78df179820ca30bd29023f58
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-09-11 22:33:56 +0300

    Implement responsive design with ScreenUtil and configure MaterialApp.router for localization and theming

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,44 @@
+import 'package:MatchIn/core/routing/app_router.dart';
+import 'package:MatchIn/core/theme/light_theme.dart' as theme;
+import 'package:MatchIn/core/utils/app_constants.dart';
+import 'package:MatchIn/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MatchIn extends StatelessWidget {
   const MatchIn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false);
+    //=== screenUtil init for responsive design ===
+    return ScreenUtilInit(
+      //TODO: change design size
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+
+      // === Material App with router ===
+      child: MaterialApp.router(
+        // === language ===
+        locale: const Locale(AppConstants.languageCode),
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+
+        // === debug banner ===
+        debugShowCheckedModeBanner: false,
+
+        // === theme ===
+        theme: theme.lightTheme,
+
+        // === router ===
+        routerConfig: AppRouter.router,
+      ),
+    );
   }
 }
