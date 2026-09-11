06c652a9f9c09ae3a1cdb05486d812d461cd656c
Ahmed Eltantawi <137333746+ahmed-eltantawi@users.noreply.github.com>
2026-09-11 20:27:57 +0300

    Add routing implementation with AppRouter and AppRoutes classes

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:go_router/go_router.dart';
+import 'package:skill_match/core/routing/app_routes.dart';
+
+abstract final class AppRouter {
+  //* --- Global Transition ---
+  static CustomTransitionPage<dynamic> _buildTransitionPage({
+    required GoRouterState state,
+    required Widget child,
+  }) {
+    return CustomTransitionPage(
+      key: state.pageKey,
+      child: child,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return FadeTransition(opacity: animation, child: child);
+      },
+    );
+  }
+
+  static final router = GoRouter(
+    routes: [
+      //TODO: add routes
+    ],
+  );
+}
---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,8 @@
+abstract final class AppRoutes {
+  static const kSplashView = '/';
+  static const kOnboardingView = '/onboarding';
+  static const kHomeView = '/home';
+  static const kLoginView = '/login';
+  static const kRegisterView = '/register';
+  static const kForgetPasswordView = '/forgotPassword';
+}
